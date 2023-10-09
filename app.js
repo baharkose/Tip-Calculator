8249520fb1619209914eb59e538bd815857c9f13
baharkose <baharkse17@gmail.com>
2023-10-09 19:32:22 +0300

    added arrays and functions

--- app.js
+++ app.js
@@ -12,6 +12,17 @@
 
 // TEST DATA: 125, 555, and 44.
 
-
-
 // 👋 OPTIONAL: You can watch my solution in video format in the next lecture
+
+function calcTip(bill){
+    let tip = bill >=50 && bill <= 300 ? bill*15/100 : bill*20/100;
+    return tip;
+}
+
+let bills = [125, 555, 44];
+
+let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+let totals = [(bills[0]+tips[0]),(bills[1]+tips[1]),(bills[2]+tips[2])];
+
+console.log(tips);
+console.log(totals);
